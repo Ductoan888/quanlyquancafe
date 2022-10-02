f86b160cae2d14b1055e17f4afd0821d4fe6f8d2
Ductoan888 <112314815+Ductoan888@users.noreply.github.com>
2022-10-02 10:05:12 +0700

    first commit

---
+++ src/Utilities/DBUtility.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author huyparody
+ */
+public class DBUtility {
+
+    public static Connection openConnection() {
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/db_ql_quancafe?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException er) {
+        }
+        return con;
+    }
+
+    public static void closeAll(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBUtility.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBUtility.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBUtility.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
